8ee5ff5ac042c76c3bd0c77a1fc5c263e3d17e9f
Tak-John Cheung <tak_john@hotmail.com>
2021-06-30 13:05:48 +0100

    update game.ts

--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,34 @@
+
+// Base
+const base = new Entity()
+base.addComponent(new GLTFShape("models/baseDarkWithCollider.glb"))
+base.addComponent(new Transform({ scale: new Vector3(2, 1, 2)}))
+engine.addEntity(base)
+
+// Screen
+const screenTransform = new Entity()
+screenTransform.addComponent(new Transform({ position: new Vector3(16, 8, 24) }))
+engine.addEntity(screenTransform)
+
 const screen = new Entity()
 screen.addComponent(new PlaneShape())
-screen.addComponent(new Transform({ position: new Vector3(8, 2, 8) }))
-screen.getComponent(Transform).scale.set(1.98, 1.28, 1)
-engine.addEntity(screen)
+screen.addComponent(new Transform({ scale: new Vector3(19.2, 10.8, 1) }))
+screen.getComponent(Transform).rotate(Vector3.Up(), 180)
+screen.setParent(screenTransform)
 
-const videoClip = new VideoClip("https://138-68-166-247.nip.io/live/test/index.m3u8")
+screenTransform.getComponent(Transform).scale.setAll(1) // You can change the scale of the screen here...
 
+// Video stream link from Vimeo
+const videoClip = new VideoClip("https://player.vimeo.com/external/552481870.m3u8?s=c312c8533f97e808fccc92b0510b085c8122a875")
 const videoTexture = new VideoTexture(videoClip)
 videoTexture.play()
 videoTexture.loop = true
 
+// Adjust screen material to increase the brightness and clarity
 const screenMaterial = new Material()
 screenMaterial.albedoTexture = videoTexture
 screenMaterial.emissiveTexture = videoTexture
 screenMaterial.emissiveColor = Color3.White()
-screenMaterial.emissiveIntensity = 0.5
+screenMaterial.emissiveIntensity = 0.6
 screenMaterial.roughness = 1.0
-
 screen.addComponent(screenMaterial)
