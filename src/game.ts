0790018c77810865121cefe7336e3842f5a4e178
Leandro Mendoza <lea.mendoza97@gmail.com>
2022-03-21 15:10:34 -0300

    chore: add eslint, ci, and clean (#5)
    * add @dcl/eslint-config
    remove bin and package-lock
    add ci to build and lint (testing)
    bump ecs dependencies to latest
    
    * add ci
    
    * fix lint
    
    * add .npmrc and engines
    
    * use sdk lint

--- src/game.ts
+++ src/game.ts
@@ -1,23 +1,25 @@
 // Base
 const base = new Entity()
-base.addComponent(new GLTFShape("models/baseDarkWithCollider.glb"))
-base.addComponent(new Transform({ scale: new Vector3(2, 1, 2)}))
+base.addComponent(new GLTFShape('models/baseDarkWithCollider.glb'))
+base.addComponent(new Transform({ scale: new Vector3(2, 1, 2) }))
 engine.addEntity(base)
 
 // Seating
 const seat = new Entity()
-seat.addComponent(new GLTFShape("models/seats.glb"))
-seat.addComponent(new Transform({ position: new Vector3(16, 0.05, 10)}))
+seat.addComponent(new GLTFShape('models/seats.glb'))
+seat.addComponent(new Transform({ position: new Vector3(16, 0.05, 10) }))
 engine.addEntity(seat)
 
 const screenBody = new Entity()
-screenBody.addComponent(new GLTFShape("models/screen.glb"))
-screenBody.addComponent(new Transform({ position: new Vector3(16, 0.05, 16)}))
+screenBody.addComponent(new GLTFShape('models/screen.glb'))
+screenBody.addComponent(new Transform({ position: new Vector3(16, 0.05, 16) }))
 engine.addEntity(screenBody)
 
 // Screen
 const screenTransform = new Entity()
-screenTransform.addComponent(new Transform({ position: new Vector3(0, 6.15, 5) }))
+screenTransform.addComponent(
+  new Transform({ position: new Vector3(0, 6.15, 5) })
+)
 screenTransform.getComponent(Transform).rotate(Vector3.Right(), -15)
 screenTransform.setParent(screenBody)
 
@@ -30,7 +32,9 @@ screen.setParent(screenTransform)
 screenTransform.getComponent(Transform).scale.setAll(0.625) // You can change the scale of the screen here...
 
 // Video stream link from Vimeo
-const videoClip = new VideoClip("https://player.vimeo.com/external/552481870.m3u8?s=c312c8533f97e808fccc92b0510b085c8122a875")
+const videoClip = new VideoClip(
+  'https://player.vimeo.com/external/552481870.m3u8?s=c312c8533f97e808fccc92b0510b085c8122a875'
+)
 const videoTexture = new VideoTexture(videoClip)
 videoTexture.play()
 videoTexture.loop = true
